d13ccef36db13b76b4a392c2ba4d054711430546
buğra uysal <uysalbugra134@gmail.com>
2024-11-14 02:56:39 +0300

    E-commerce sites have been reached and the cheapest products will be shown later.

--- howtimegoal.py
+++ howtimegoal.py
@@ -3,37 +3,34 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import tkinter as tk
+import requests
+from bs4 import BeautifulSoup
+import webbrowser
 
 aa=tk.Tk()
 aa.title("goals")
 aa.geometry("500x500")
 
-secenek=["ev","araba","para biriktirme"]
-ılk=tk.StringVar()
-ılk.set(secenek[0])
 
-oc=tk.OptionMenu(aa ,ılk ,*secenek )
-oc.pack()
-""" price """
-one=tk.Entry(aa )
-one.pack()
-"how much mounth"
-two=tk.Entry(aa)
-two.pack()
 
-tri=tk.Entry(aa)
-tri.pack()
-
-bb=tk.Label(aa)
-bb.pack()
 def  hsp():
-    al=one.get()
-    bb.config(text=al)
+    gt=search.get()
     
+    """  url """
+    hb=f"https://www.hepsiburada.com/ara?q={gt}"
+    ty=f"https://www.trendyol.com/sr?q={gt}&qt={gt}&st={gt}&os=1"
+    amz=f"https://www.amazon.com.tr/s?k={gt}&__mk_tr_TR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=KRELF70G1RQD&sprefix=f%C4%B1nd%C4%B1k%2Caps%2C181&ref=nb_sb_noss_1"
+    gogle=f"https://www.google.com/search?q={gt}"
+    
+    webbrowser.open(hb)
+    
+  
+
+search=tk.Entry(aa)
+search.pack()
 
 
-
-cal = tk.Button(aa, text="calculate", command=hsp)
+cal = tk.Button(aa, text="find", command=hsp)
 cal.pack()
 
 
